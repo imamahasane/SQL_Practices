bae6ff80753ae6431bc76aae2934246736dfb300
Md Imam Ahasan <emamahasane@gmail.com>
2022-01-17 07:09:36 +0600

    Add files via upload

--- 3_joins.sql
+++ 3_joins.sql
@@ -1,3 +1,33 @@
-select * from employees;
+create database sql_joins;
 show databases;
-show tables from sql_intro;
+use sql_joins;
+
+create table cricket (
+cricket_id int auto_increment,
+name varchar(30), 
+primary key(cricket_id));
+
+
+create table football (
+football_id int auto_increment,
+name varchar(30), 
+primary key(football_id));
+
+insert into cricket (name)
+value ('Imam'), ('Michael'), ('Samiul'), ('Hayden'), ('Fleming');
+
+select * from cricket;
+
+insert into football (name)
+value ('Imam'), ('Samiul'), ('Noor'), ('Anan'), ('Shotez');
+
+select * from football;
+
+select * from cricket as c inner join
+football as f on c.name = f.name;
+# or
+select * from cricket inner join football on cricket.name = football.name;
+
+
+
+
